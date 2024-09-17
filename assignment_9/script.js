98a97b2eca1c949ae2b67413f5cc48417b668878
Shafique Ur Rehman <160214626+SHAFIQUEKHANZADA@users.noreply.github.com>
2024-09-17 21:13:59 +0500

    Assignment_9

---
+++ assignment_9/script.js
@@ -0,0 +1,6 @@
+const firstName = prompt("Enter your first name");
+const lastName = prompt("Enter your last name");
+const age = prompt("Enter your age");
+
+
+alert(`Hello ${firstName} ${lastName}, You are ${age} years old`);
